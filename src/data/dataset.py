fix: fix masking and visualization

src/data/dataset.py
@@ -1,6 +1,7 @@
 import json
 import os
 from abc import ABC, abstractmethod
+from typing import Type
 
 import torch
 from PIL import Image
@@ -12,7 +13,11 @@ from src.utils import CONSTANTS
 class DETRDataset(torch.utils.data.Dataset, ABC):
     @abstractmethod
     def __init__(self):
-        pass
+        self.images = []
+        self.boxes = []
+        self.labels = []
+        self.transform = T.ToTensor()
+        self.image_metadata = {}
 
     def __len__(self):
         return len(self.images)
@@ -38,7 +43,7 @@ class SFCHDDataset(DETRDataset):
     def __init__(self, folder_path, partition: str = "train"):
         super().__init__()
         self._folder_path = folder_path
-        pass
+        self.partition = partition
 
 
 class CPPE5Dataset(DETRDataset):
@@ -184,15 +189,16 @@ def collate_fn(batch):
     images = [item[0] for item in batch]  # various sizes
     targets = [item[1] for item in batch]  # various number of boxes for each image
 
+    # Batch images
+    max_w = max([img.shape[2] for img in images])
+    max_h = max([img.shape[1] for img in images])
+
     targets = {
-        "boxes": [t["boxes"] for t in targets],
+        "boxes": [t["boxes"] / torch.tensor([max_w, max_h, max_w, max_h], dtype=torch.float32) for t in targets],
         "labels": [t["labels"] for t in targets],
         "image_id": [t["image_id"] for t in targets],
     }
 
-    # Batch images
-    max_w = max([img.shape[2] for img in images])
-    max_h = max([img.shape[1] for img in images])
     batch_size = len(images)
     batched_imgs = torch.zeros(
         (batch_size, 3, max_h, max_w), dtype=images[0].dtype

src/data/transforms.py
@@ -490,7 +490,7 @@ class Normalize(BaseTransform):
         h, w = img.shape[-2:]
         if "boxes" in new_target:
             boxes = new_target["boxes"]
-            boxes = boxes / torch.tensor([w, h, w, h], dtype=torch.float32)
+            # boxes = boxes / torch.tensor([w, h, w, h], dtype=torch.float32)
             new_target["boxes"] = boxes
         return new_img, new_target
 
@@ -533,6 +533,6 @@ class UnNormalize(BaseTransform):
         h, w = img.shape[-2:]
         if "boxes" in new_target:
             boxes = new_target["boxes"]
-            boxes = boxes * torch.tensor([w, h, w, h], dtype=torch.float32)
+            # boxes = boxes * torch.tensor([w, h, w, h], dtype=torch.float32)
             new_target["boxes"] = boxes
         return new_img, new_target

src/data/utils.py
@@ -49,21 +49,25 @@ def visualize_output(
 
     for idx, image_id in enumerate(image_ids):
         img = imgs_np[idx]
+        w, h = img.shape[2], img.shape[1]
+
+        gt_visualized = gt_bboxes[idx] * torch.tensor([w, h, w, h], dtype=torch.float32) if len(gt_bboxes[idx]) > 0 else gt_bboxes[idx]
+        out_visualized = out_bboxes[idx] * torch.tensor([w, h, w, h], dtype=torch.float32) if len(out_bboxes[idx]) > 0 else out_bboxes[idx]
 
         if unormalize:
             # Concatnate gt_bboxes and out_bboxes for unnormalization
             if len(gt_bboxes[idx]) > 0 and len(out_bboxes[idx]) > 0:
-                all_bboxes = torch.cat((gt_bboxes[idx], out_bboxes[idx]), dim=0)
+                all_bboxes = torch.cat((gt_visualized, out_visualized), dim=0)
             else:
-                all_bboxes = gt_bboxes[idx] if len(gt_bboxes[idx]) > 0 else out_bboxes[idx]
+                all_bboxes = gt_visualized if len(gt_visualized) > 0 else out_visualized
 
             img, all_bboxes = unnormalize(img, {"boxes": all_bboxes})
             all_bboxes = all_bboxes["boxes"]
 
             # Split back to gt_bboxes and out_bboxes
-            if len(gt_bboxes[idx]) > 0 and len(out_bboxes[idx]) > 0:
-                gt_visualized = all_bboxes[: len(gt_bboxes[idx])]
-                out_visualized = all_bboxes[len(gt_bboxes[idx]) :]
+            if len(gt_visualized) > 0 and len(out_visualized) > 0:
+                gt_visualized = all_bboxes[: len(gt_visualized)]
+                out_visualized = all_bboxes[len(gt_visualized) :]
             elif len(gt_bboxes[idx]) > 0:
                 gt_visualized = all_bboxes
                 out_visualized = []
@@ -74,8 +78,6 @@ def visualize_output(
                 gt_visualized = []
                 out_visualized = []
         else:
-            gt_visualized = gt_bboxes[idx]
-            out_visualized = out_bboxes[idx]
             img = copy.deepcopy(img)
 
         img = img.permute(1, 2, 0).numpy()